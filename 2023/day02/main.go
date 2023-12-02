853ba30401770158ad1c59c908a094e122a76d65
Hakim <hakim.rouatbi@gmail.com>
2023-12-03 00:41:42 +0100

    Complete day 2' : parts 1 and 2

--- 2023/day02/main.go
+++ 2023/day02/main.go
@@ -9,6 +9,11 @@ import (
 )
 
 func main() {
+    // part1()
+    part2()
+}
+
+func part1() {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
@@ -51,4 +56,38 @@ func main() {
 	}
 
 	fmt.Println("------ The Answer My Friend is: ", gameCount, " -------")
+}
+
+func part2() {
+    file, _ := os.Open("input.txt")
+    defer file.Close()
+
+    // This is the variable that will hold the final result
+    totalPower := 0
+
+    scanner := bufio.NewScanner(file)
+    //Foreach game line
+    for scanner.Scan() {
+        setsInfo := strings.Split(scanner.Text(), ": ")[1]
+        minCubes := make(map[string]int)
+        minCubes["red"] = 0
+        minCubes["green"] = 0
+        minCubes["blue"] = 0
+        //Foreach game set
+        for _, set := range strings.Split(setsInfo, "; ") {
+            for _, col := range strings.Split(set, ", ") {
+				nb, _ := strconv.Atoi(strings.Split(col, " ")[0])
+				colName := strings.Split(col, " ")[1]
+                if nb > minCubes[colName] {
+                    minCubes[colName] = nb
+                }
+            }
+        }
+        gamePower := 1
+        for _, v := range(minCubes) {
+            gamePower *= v
+        }
+        totalPower += gamePower
+    }
+	fmt.Println("------ The Answer My Friend is: ", totalPower, " -------")
 }
