ba7397249fe1af24a4d64525482ee180d9ef9627
Hakim <hakim.rouatbi@gmail.com>
2023-12-04 22:31:07 +0100

    Solve day4's part 2

--- 2023/day04/main.go
+++ 2023/day04/main.go
@@ -9,8 +9,8 @@ import (
 )
 
 func main() {
-    part1()
-    // part2()
+    // part1()
+    part2()
 }
 
 func part1() {
@@ -40,5 +40,43 @@ func part1() {
 }
 
 func part2() {
+    file, _ := os.ReadFile("input.txt")
+    lines := strings.Split(strings.TrimSpace(string(file)), "\n")
+
+    cardCount := make(map[int]int)
+    // Foreach card (line in the input file)
+    for i, l := range lines {
+        if _, exists := cardCount[i]; !exists {
+            cardCount[i] = 1
+        } else {
+            cardCount[i]++
+        }
+        cardNums := strings.Fields(strings.TrimSpace(strings.Split(strings.Split(l, ": ")[1], "|")[0]))
+        yourNums := strings.Fields(strings.TrimSpace(strings.Split(strings.Split(l, ": ")[1], "|")[1]))
+        cardScore := 0
+        for _, yn := range yourNums {
+            if slices.Contains(cardNums, yn) {
+                cardScore++
+            }
+        }
+        for n := 0; n < cardCount[i]; n++ {
+            for j := 0; j < cardScore; j++ {
+                // Out of bounds
+                if i + j + 1 > len(lines) - 1 {
+                    break
+                }
+                if _, exists := cardCount[i+j+1]; !exists {
+                    cardCount[i+j+1] = 1
+                } else {
+                    cardCount[i+j+1]++
+                }
+            }
+        }
+    }
+    totalScore := 0
+    for _, v := range cardCount {
+        totalScore += v
+    }
+    fmt.Println("------ The answer my friend is : ", totalScore, " ------")
 }
 
