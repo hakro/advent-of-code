a5e84f4a33a62073f3321abd203c97e4fb7c7f42
Hakim <hakim.rouatbi@gmail.com>
2023-12-04 03:03:00 +0100

    Solve day 3 - part 2

--- 2023/day03/main.go
+++ 2023/day03/main.go
@@ -10,8 +10,8 @@ import (
 )
 
 func main() {
-    part1()
-    // part2()
+    // part1()
+    part2()
 }
 
 func part1() {
@@ -101,6 +101,98 @@ func part1() {
     fmt.Println("----- The answer my friend, is : ", totalCount, " -----")
 }
 
+func part2() {
+    starMap := make(map[string][]int) //Keys are formatted as line-row as star coordinates, values will be the surrounding numbers
+    file, _ := os.ReadFile("input.txt")
+    pat := regexp.MustCompile(`\d+`)
+    lines := strings.Split(strings.TrimSpace(string(file)), "\n")
+    // Foreach line in the file
+    for i, l := range lines {
+        nums := pat.FindAllString(l, -1)
+        positions := pat.FindAllStringIndex(l, -1)
+        // Foreach number matched
+        for j, n := range nums {
+            firstIndex := positions[j][0]
+            lastIndex := firstIndex + len(n) - 1
+            // Check the left side
+            if firstIndex > 0 {
+                // Check direct left
+                if isSymbolStar(l[firstIndex - 1]) {
+                    val, _ := strconv.Atoi(n)
+                    starMap[string(i) + "-" + string(firstIndex - 1)] = append(starMap[string(i) + "-" + string(firstIndex - 1)], val)
+                    continue
+                }
+                // Check upper left diagonally
+                if i > 0 {
+                    if isSymbolStar(lines[i-1][firstIndex - 1]) {
+                        val, _ := strconv.Atoi(n)
+                        starMap[string(i-1) + "-" + string(firstIndex - 1)] = append(starMap[string(i-1) + "-" + string(firstIndex - 1)], val)
+                        continue
+                    }
+                }
+                // Check lower left diagonally
+                if i < len(lines) - 1 {
+                    if isSymbolStar(lines[i+1][firstIndex - 1]) {
+                        val, _ := strconv.Atoi(n)
+                        starMap[string(i+1) + "-" + string(firstIndex - 1)] = append(starMap[string(i+1) + "-" + string(firstIndex - 1)], val)
+                        continue
+                    }
+                }
+            }
+            // Check the right side
+            if lastIndex < len(l) - 1 {
+                // Check direct right
+                if isSymbolStar(l[lastIndex + 1]) {
+                    val, _ := strconv.Atoi(n)
+                    starMap[string(i) + "-" + string(lastIndex + 1)] = append(starMap[string(i) + "-" + string(lastIndex + 1)], val)
+                    continue
+                }
+                // Check upper right diagonally
+                if i > 0 {
+                    if isSymbolStar(lines[i-1][lastIndex + 1]) {
+                        val, _ := strconv.Atoi(n)
+                        starMap[string(i-1) + "-" + string(lastIndex + 1)] = append(starMap[string(i-1) + "-" + string(lastIndex + 1)], val)
+                        continue
+                    }
+                }
+                // Check lower right diagonally
+                if i < len(lines) - 2 {
+                    if isSymbolStar(lines[i+1][lastIndex + 1]) {
+                        val, _ := strconv.Atoi(n)
+                        starMap[string(i+1) + "-" + string(lastIndex + 1)] = append(starMap[string(i+1) + "-" + string(lastIndex + 1)], val)
+                        continue
+                    }
+                }
+            }
+            // Check the upper and lower sides
+            for u := firstIndex; u <= lastIndex; u++ {
+                if i > 0 {
+                    if isSymbolStar(lines[i-1][u]) {
+                        val, _ := strconv.Atoi(n)
+                        starMap[string(i-1) + "-" + string(u)] = append(starMap[string(i-1) + "-" + string(u)], val)
+                        break
+                    }
+                }
+                if i < len(lines) - 1 {
+                    if isSymbolStar(lines[i+1][u]) {
+                        val, _ := strconv.Atoi(n)
+                        starMap[string(i+1) + "-" + string(u)] = append(starMap[string(i+1) + "-" + string(u)], val)
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    totalCount := 0
+    for _, v := range(starMap) {
+        if len(v) == 2 {
+            totalCount += v[0] * v[1]
+        }
+    }
+    fmt.Println("----- The answer my friend, is : ", totalCount, " -----")
+}
+
 func isSymbol(b byte) bool {
     if !unicode.IsDigit(rune(b)) && string(b) != "." {
         return true
@@ -108,5 +200,9 @@ func isSymbol(b byte) bool {
     return false
 }
 
-func part2() {
+func isSymbolStar(b byte) bool {
+    if string(b) == "*" {
+        return true
+    }
+    return false
 }
