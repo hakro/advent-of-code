b3bef8e86daa9d1c5ded3559aac2d44e4803ca36
Hakim <hakim.rouatbi@gmail.com>
2023-12-08 21:22:20 +0100

    Brute force day 8 part 2 solution. Should use LCM or chinese remainer theorem

--- 2023/day08/main.go
+++ 2023/day08/main.go
@@ -26,16 +26,47 @@ func part1() {
     steps := 0
     currentDir := "AAA"
 
+    currentDirPart2 := []string{}
     scanner.Scan() //Skip empty line
     elems := make(map[string][2]string)
     for scanner.Scan() {
         l := strings.Split(scanner.Text(), " = ")
+        if strings.HasSuffix(l[0], "A") {
+            currentDirPart2 = append(currentDirPart2, l[0])
+        }
         elems[l[0]] = [2]string{
             strings.TrimPrefix(strings.Split(l[1], ", ")[0], "("),
             strings.TrimSuffix(strings.Split(l[1], ", ")[1], ")"),
         }
     }
+    part2 := true
     for true {
+        if part2 {
+            // Part2
+            nbZ := 0 // Number of things ending in Z
+            for _, e := range currentDirPart2 {
+                if strings.HasSuffix(e, "Z") {
+                    nbZ++
+                }
+            }
+            fmt.Println(steps)
+            // All of directions en in Z, terminate
+            if nbZ == len(currentDirPart2) {
+                break
+            }
+            for i, el := range currentDirPart2 {
+                switch getDirectionByStep(steps) {
+                case "L" :
+                    currentDirPart2[i] = elems[el][0]
+                case "R" :
+                    currentDirPart2[i] = elems[el][1]
+                }
+            }
+            steps++
+            continue
+        }
+
+        // Part 1
         if currentDir == "ZZZ" {
             break
         }
