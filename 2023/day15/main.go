156b871008730aabf9c412eb8db969e468b6c0fc
Hakim <hakim.rouatbi@gmail.com>
2023-12-15 20:42:53 +0100

    Solve day 15's part 2

--- 2023/day15/main.go
+++ 2023/day15/main.go
@@ -4,6 +4,7 @@ import (
     "os"
     "fmt"
     "strings"
+    "strconv"
 )
 
 func main() {
@@ -13,7 +14,7 @@ func main() {
 
 func hash(s string) int {
     current := 0
-    for _, c := range strings.TrimSpace(s) {
+    for _, c := range s {
         current = ((current + int(c)) * 17) % 256
     }
     return current
@@ -21,14 +22,54 @@ func hash(s string) int {
 
 func part1() {
     file, _ := os.ReadFile("input.txt")
-    values := strings.Split(string(file), ",")
+    values := strings.Split(strings.TrimSpace(string(file)), ",")
 
-    total := 0
+    // Used for part 2
+    boxes := map[int][]string{}
+
+    totalPart1 := 0
     for _, v := range values {
-        total += hash(v)
+        h := hash(v)
+        totalPart1 += h
+
+        h = hash(strings.Split(strings.Split(v, "=")[0], "-")[0])
+        // Part2
+        if strings.HasSuffix(v, "-") {
+            // for each lens in the box
+            for i, l := range boxes[h] {
+                if strings.Split(v, "-")[0] == strings.Split(l, "=")[0] {
+                    // remove
+                    boxes[h] = append(boxes[h][:i], boxes[h][i+1:]...)
+                }
+            }
+        }
+
+        if len(strings.Split(v, "=")) == 2 {
+            added := false
+            for i, l := range boxes[h] {
+                if strings.Split(v, "=")[0] == strings.Split(l, "=")[0] {
+                    added = true
+                    // boxes[h] = slices.Insert(boxes[h], i, v)
+                    boxes[h][i] = v
+                    break
+                }
+            }
+            if !added {
+                    boxes[h] = append(boxes[h], v)
+            }
+        }
     }
 
-    fmt.Println("----- The Part 1 Answer My Friend Is :", total, "-----")
+    fmt.Println("----- The Part 1 Answer My Friend Is :", totalPart1, "-----")
+    totalPart2 := 0
+    for i, b := range boxes {
+        for j, l := range b {
+            // Focus Power
+            focalLength, _ := strconv.Atoi(strings.Split(l, "=")[1])
+            totalPart2 += (i + 1) * (j + 1) * focalLength
+        }
+    }
+    fmt.Println("----- The Part 2 Answer My Friend Is :", totalPart2, "-----")
 }
 
 func part2() {
