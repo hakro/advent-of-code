d83063df91ff45cedf498c9a8d3c926de2c77c1b
hakro <hakim.rouatbi@gmail.com>
2024-12-05 00:20:00 +0100

    Day4 - Part1: check diagonals

--- 2024/day04/main.c
+++ 2024/day04/main.c
@@ -8,18 +8,24 @@
 
 // Check the table at position (l, c).
 // Use max to check you don't get out of array bounds
-int check_hor_right(char **table, size_t l, size_t c, size_t max);
-int check_hor_left(char **table, size_t l, size_t c);
+int check_right(char **table, size_t l, size_t c, size_t max);
+int check_left(char **table, size_t l, size_t c);
 int check_down(char **table, size_t l, size_t c, size_t max);
 int check_up(char **table, size_t l, size_t c);
+int check_diag_up_left(char **table, size_t l, size_t c, size_t max);
+int check_diag_up_right(char **table, size_t l, size_t c, size_t max);
+int check_diag_down_left(char **table, size_t l, size_t c, size_t max);
+int check_diag_down_right(char **table, size_t l, size_t c, size_t max);
+int get_all_xmases_for_pos(char **table, size_t l, size_t c, size_t max); //Returns the total xmas words
 
 int main() {
 
-	FILE *f = fopen("input-example.txt", "r");
+	FILE *f = fopen("input.txt", "r");
 	if (f == NULL) {
 		printf("err: cant read input file");
 		return 1;
 	}
+	int solution = 0;
 	size_t nb_lines = 0;
 	size_t nb_columns = 0;
 
@@ -46,14 +52,19 @@ int main() {
 			if (table[l][c] != 'X') {
 				continue;
 			}
-			/* printf("--> %d\n", check_hor_right(table, l, c, nb_columns)); */
-			/* printf("--> %d\n", check_hor_left(table, l, c)); */
+			/* printf("--> %d\n", check_right(table, l, c, nb_columns)); */
+			/* printf("--> %d\n", check_left(table, l, c)); */
 			/* printf("--> %d\n", check_down(table, l, c, nb_columns)); */
-			printf("--> %d\n", check_up(table, l, c));
+			/* printf("--> %d\n", check_up(table, l, c)); */
+			/* printf("--> %d\n", check_diag_up_left(table, l, c, nb_columns)); */
+			/* printf("--> %d\n", check_diag_up_right(table, l, c, nb_columns)); */
+			/* printf("--> %d\n", check_diag_down_left(table, l, c, nb_columns)); */
+			/* printf("--> %d\n", check_diag_down_right(table, l, c, nb_columns)); */
+			solution += get_all_xmases_for_pos(table, l, c, nb_columns);
 		}
 	}
-
-
+	printf("Part1 solution : %d\n", solution);
+	
 
 	// -------- Clean up ------------ //
 	for (int i = 0; i < nb_lines; i++) {
@@ -65,24 +76,22 @@ int main() {
 }
 
 
-int check_hor_right(char **table, size_t l, size_t c, size_t max) {
+int check_right(char **table, size_t l, size_t c, size_t max) {
 	if (c <= max - XMAS_LEN) {
 		char dst[XMAS_LEN];
 		strncpy(dst, table[l] + c, XMAS_LEN);
 		if (strcmp(dst, "XMAS") == 0) {
-			printf("-- found : %ld / %ld\n", l, c);
 			return 1;
 		}
 	} 
 	return 0;
 }
 
-int check_hor_left(char **table, size_t l, size_t c) {
+int check_left(char **table, size_t l, size_t c) {
 	if (c >= XMAS_LEN - 1) {
 		char dst[XMAS_LEN];
 		strncpy(dst, table[l] + c - XMAS_LEN + 1, XMAS_LEN);
 		if (strcmp(dst, "SAMX") == 0) {
-			printf("-- found : %ld / %ld\n", l, c);
 			return 1;
 		}
 	} 
@@ -98,7 +107,6 @@ int check_down(char **table, size_t l, size_t c, size_t max) {
 		dst[3] = table[l+3][c];
 
 		if (strcmp(dst, "XMAS") == 0) {
-			printf("-- found : %ld / %ld\n", l, c);
 			return 1;
 		}
 	}	
@@ -114,9 +122,82 @@ int check_up(char **table, size_t l, size_t c) {
 		dst[3] = table[l-3][c];
 
 		if (strcmp(dst, "XMAS") == 0) {
-			printf("-- found : %ld / %ld\n", l, c);
 			return 1;
 		}
 	}	
 	return 0;
+}
+
+int check_diag_up_left(char **table, size_t l, size_t c, size_t max) {
+	if (l >= XMAS_LEN - 1 && c >= XMAS_LEN - 1) {
+		char dst[XMAS_LEN];
+		dst[0] = table[l][c];
+		dst[1] = table[l-1][c-1];
+		dst[2] = table[l-2][c-2];
+		dst[3] = table[l-3][c-3];
+
+		if (strcmp(dst, "XMAS") == 0) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int check_diag_up_right(char **table, size_t l, size_t c, size_t max) {
+	if (l >= XMAS_LEN - 1 && c <= max - XMAS_LEN) {
+		char dst[XMAS_LEN];
+		dst[0] = table[l][c];
+		dst[1] = table[l-1][c+1];
+		dst[2] = table[l-2][c+2];
+		dst[3] = table[l-3][c+3];
+
+		if (strcmp(dst, "XMAS") == 0) {
+			return 1;
+		}
+	}
+	return 0;
+}
+int check_diag_down_left(char **table, size_t l, size_t c, size_t max) {
+	if (l <= max - XMAS_LEN && c >= XMAS_LEN - 1) {
+		char dst[XMAS_LEN];
+		dst[0] = table[l][c];
+		dst[1] = table[l+1][c-1];
+		dst[2] = table[l+2][c-2];
+		dst[3] = table[l+3][c-3];
+
+		if (strcmp(dst, "XMAS") == 0) {
+			return 1;
+		}
+	}
+	return 0;
+}
+int check_diag_down_right(char **table, size_t l, size_t c, size_t max) {
+	if (l <= max - XMAS_LEN && c <= max - XMAS_LEN) {
+		char dst[XMAS_LEN];
+		dst[0] = table[l][c];
+		dst[1] = table[l+1][c+1];
+		dst[2] = table[l+2][c+2];
+		dst[3] = table[l+3][c+3];
+
+		if (strcmp(dst, "XMAS") == 0) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int get_all_xmases_for_pos(char **table, size_t l, size_t c, size_t max) {
+
+	int total = 0;
+
+	total += check_right(table, l, c, max);
+	total += check_left(table, l, c);
+	total += check_down(table, l, c, max);
+	total += check_up(table, l, c);
+	total += check_diag_up_left(table, l, c, max);
+	total += check_diag_up_right(table, l, c, max);
+	total += check_diag_down_left(table, l, c, max);
+	total += check_diag_down_right(table, l, c, max);
+
+	return total;
 }
